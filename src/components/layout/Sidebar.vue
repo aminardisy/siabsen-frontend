c9b12f2fa829237e4245aa17efa1b14775f5919c
Ghiranza Athaya Hamid <ghiranza.athaya@ui.ac.id>
2026-04-20 13:48:48 +0700

    merge for lolly fe

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -51,6 +51,20 @@
         </template>
       </SidebarItem>
 
+      <SidebarItem
+        v-if="['KESISWAAN', 'SEKRETARIS'].includes(authStore.user?.role)"
+        to="/janji-temu/create"
+        label="Janji Temu"
+        :active="route.path.startsWith('/janji-temu')"
+        :isCollapsed="uiStore.isSidebarCollapsed"
+      >
+        <template #icon>
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+          </svg>
+        </template>
+      </SidebarItem>
+
       <template v-if="authStore.user?.role === 'ADMIN'">
         <div class="px-4 py-2 text-[10px] font-bold text-gray-500 uppercase tracking-widest" v-if="!uiStore.isSidebarCollapsed">
           Data Master
