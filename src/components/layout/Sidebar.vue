7e645e0d32b8a913d6593cdef9f31e2d2c5f8ff9
F00-Fighter <farrel.drmwn00@gmail.com>
2026-05-17 02:26:47 +0700

    chore: fixed sidebar duplicate item

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -259,23 +259,6 @@
         </template>
       </SidebarItem>
 
-      <SidebarItem
-          v-if="['GURU', 'KESISWAAN'].includes(authStore.user?.role)"
-          to="/rekap"
-          label="Rekap Absensi"
-          :active="route.path.startsWith('/rekap')"
-          :isCollapsed="uiStore.isSidebarCollapsed"
-        >
-          <template #icon>
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-
-            </svg>
-          </template>
-        </SidebarItem>
 
       <!-- Tombol Keluar -->
       <div class="mt-auto pb-4">
